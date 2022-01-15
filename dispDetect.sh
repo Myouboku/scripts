02c4588ca8a1460201ae2d1d1be8aab9d16329ac
Hugo Avril <hugo.avril@outlook.fr>
2022-01-15 23:18:05 +0100

    suppression while (impact systeme)

--- dispDetect.sh
+++ dispDetect.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-while true
-do
-	intern=eDP-1-1
-	extern=HDMI-1-1
+intern=eDP-1-1
+extern=HDMI-1-1
 
-	if xrandr | grep "$extern disconnected"; then
-        xrandr --output "$extern" --off --output "$intern" --mode 1920x1080
-	else
-        xrandr --output "$intern" --off --output "$extern" --mode 1920x1080 --rate 120.00
-	fi
-	sleep 5
-done
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off --output "$intern" --mode 1920x1080
+else
+    xrandr --output "$intern" --off --output "$extern" --mode 1920x1080 --rate 120.00
+fi
