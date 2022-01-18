13991d3534788a3cde8c764315159d07d9373ba4
Hugo Avril <hugo.avril@outlook.fr>
2022-01-18 01:13:38 +0100

    support carte intégrée

--- dispDetect.sh
+++ dispDetect.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-intern=eDP-1-1
-extern=HDMI-1-1
+intern1="eDP-1"
+extern1="HDMI-1"
+intern2="eDP-1-1"
+extern2="HDMI-1-1"
+noWanted="DP-1"
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --mode 1920x1080
+if xrandr | grep "$extern1 connected"; then
+    xrandr --output "$intern1" --off --output "$extern1" --mode 1920x1080 --rate 120.00
+elif xrandr | grep "$extern2 connected"; then
+    xrandr --output "$intern2" --off --output "$extern2" --mode 1920x1080 --rate 120.00
+elif xrandr | grep "$intern1 connected"; then
+    xrandr --output "$extern1" --off --output "$intern1" --mode 1920x1080
 else
-    xrandr --output "$intern" --off --output "$extern" --mode 1920x1080 --rate 120.00
+    xrandr --output "$extern2" --off --output "$intern2" --mode 1920x1080
 fi
